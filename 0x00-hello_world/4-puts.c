ed046fa3e248a6e24ec376c432c9d020af33f930
aficionado3 <curtismwarema01@gmail.com>
2022-12-09 01:44:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
-
  * Description: main - Prints out a string to stdout.
-
  * Return: Always 0 (Success).
-
  */
-
-int main(void)
-  
+int main(void) 
 {
-  
   puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+  return (0);  
 }
