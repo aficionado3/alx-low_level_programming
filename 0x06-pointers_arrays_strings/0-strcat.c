f5701ae8bd3e82d7819689cf52d9d8813ce3042a
aficionado3 <curtismwarema01@gmail.com>
2022-12-22 05:46:10 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
-* _memset - fills memory with a constant byte
-*
-* @s: pointer to memory area to fill
-* @n: the number of bytes to fill
-* @b: constant byte
-*
-* Return: a pointer to the memory area @s
+* _strcat - a function that concatenates two strings
+* @dest: copy to
+* @src: copy from
+* Return: two concat strings
 */
-char *_memset(char *s, char b, unsigned int n)
+char *_strcat(char *dest, char *src)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
+int i, n;
+for (i = 0; dest[i] != '\0'; i++)
 {
-s[i] = b;
 }
-return (s);
+for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+{
+}
+return (dest);
 }
